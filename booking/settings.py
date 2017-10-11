9b8f28de64f07547451d0b058020d3b872f87ece
Aditya Gupta <guptaadityaiitb@gmail.com>
2017-10-11 14:51:20 +0530

    Update settings.py

--- booking/settings.py
+++ booking/settings.py
@@ -31,8 +31,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'aditya.gupta@ashoka.edu.in'
-EMAIL_HOST_PASSWORD = '@Aditya123!'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 # Application definition
